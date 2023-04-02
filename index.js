7c7a3df810b769125858bf9196c9c683d2342fe1
marcos.vinicyus.c.s@gmail.com <marcos.vinicyus.c.s@gmail.com>
2023-04-02 14:11:07 -0300

    Configurando o banco e a conexão com o mesmo

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 //Importando as bibliotecas
 const express = require('express');
 const app = express();
+const connection = require('./database/database')
 
 //Setando as viwes
 app.set('view engine', 'ejs');
 
 //Setando arquivos estaticos
 app.set(express.static('public'));
+
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso")
+    }).catch((error) => {
+        console.log(error)
+    })
 
 app.get("/", (req, res) => {
     res.render("index")
