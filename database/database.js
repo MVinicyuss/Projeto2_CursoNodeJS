8e200d3c87c5631860350fafd2e90bce9f465468
marcos.vinicyus.c.s@gmail.com <marcos.vinicyus.c.s@gmail.com>
2023-04-08 23:48:26 -0300

    Alterações leves

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require ('sequelize');
 
 const connection = new Sequelize('guiapress', 'root', '1234', {
     host:       'localhost',
-    dialect:    'mysql'
+    dialect:    'mysql',
+    logging:    false
 });
 
 module.exports = connection;
