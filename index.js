04f0bf1d8e7dfc387b64ff93f5c3f90f314addb7
marcos.vinicyus.c.s@gmail.com <marcos.vinicyus.c.s@gmail.com>
2023-04-15 23:57:15 -0300

    Rota e view de exibição de um article

--- index.js
+++ index.js
@@ -40,6 +40,23 @@ app.get("/", (req, res) => {
     })
 })
 
+app.get("/:slug", (req, res) => {
+    let slug = req.params.slug
+    Article.findOne({
+        where:{
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            res.render("article", {article: article});
+        }else{
+            res.redirect("/")
+        }
+    }).catch( err => {
+        res.redirect("/")
+    })
+})
+
 // Porta do servidor
 app.listen(8080, () => {
     console.log("Servidor rodando")
